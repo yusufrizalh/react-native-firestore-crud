13e3ae1ff484d9c6759d62e1f6f9fd686c5e90cf
Yusuf Rizal <yusufrizalh@gmail.com>
2020-11-12 14:49:01 +0700

    react native firestore crud

--- database/firebaseDb.js
+++ database/firebaseDb.js
@@ -2,13 +2,14 @@ import * as firebase from 'firebase';
 import firestore from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    authDomain: "xxxxxxxxx-00000.firebaseapp.com",
-    databaseURL: "https://xxxxxxxxx-00000.firebaseio.com",
-    projectId: "xxxxxxxxx-00000",
-    storageBucket: "xxxxxxxxx-00000.appspot.com",
-    messagingSenderId: "000000000000000",
-    appId: "1:000000000000000:web:000000000000000"
+  apiKey: "AIzaSyAYAp7PtunIfxk-BnETyel91LezAwLnwHw",
+  authDomain: "api-key-sample.firebaseapp.com",
+  databaseURL: "https://api-key-sample.firebaseio.com",
+  projectId: "api-key-sample",
+  storageBucket: "api-key-sample.appspot.com",
+  messagingSenderId: "674261490837",
+  appId: "1:674261490837:web:50ab0c4f52aa584c3acf9c",
+  measurementId: "G-2QLZLLFV1G",
 };
 
 firebase.initializeApp(firebaseConfig);
